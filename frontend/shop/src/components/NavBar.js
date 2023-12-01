8d84f6e3240e30153e81da14fa38eeccac469bea
eduardo-mw <eduardo@middleware.io>
2023-12-01 15:25:23 -0800

    add basic navbar

--- frontend/shop/src/components/NavBar.js
+++ frontend/shop/src/components/NavBar.js
@@ -1,21 +1,40 @@
 import React from "react";
 
 const NavBar = () => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">Shop</a>
-        <div className="collapse navbar-collapse">
-            <ul className="navbar-nav mr-auto">
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Mobi eShop
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
             <li className="nav-item">
-                <a href="/products" className="nav-link">Products</a>
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="/checkout">Checkout</a>
+              <a className="nav-link" href="/products">
+                Products
+              </a>
             </li>
-            </ul>
+          </ul>
         </div>
-        </nav>
-    );
-    };
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
