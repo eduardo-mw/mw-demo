ba0a34e3fd0c57f12a59c305ea6156a6305cdd38
eduardo-mw <eduardo@middleware.io>
2023-12-08 13:53:19 -0800

    add global products and usercart state

--- frontend/shop/src/App.js
+++ frontend/shop/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
@@ -11,6 +11,9 @@ import Home from "./pages";
 import ThankYou from "./pages/thankyou";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [userCart, setUserCart] = useState([]);
+
   return (
     <Router>
       <NavBar/>
