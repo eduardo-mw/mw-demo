2e9898b7b5039da2650ea2316729e67b1d66a877
eduardo-mw <eduardo@middleware.io>
2023-12-10 18:05:14 -0800

    make all links home page

--- frontend/shop/src/components/NavBar/PrimaryNav.js
+++ frontend/shop/src/components/NavBar/PrimaryNav.js
@@ -6,7 +6,7 @@ const PrimaryNav = () => {
 		<div className="container">
 			<div className="offcanvas offcanvas-start" tabIndex="-1" id="navbar-default" aria-labelledby="navbar-defaultLabel">
 				<div className="offcanvas-header pb-1">
-					<a href="./index.html"><img src="./assets/images/logo/freshcart-logo.svg" alt="eCommerce HTML Template"/></a>
+					<a href="/"><img src="./assets/images/logo/freshcart-logo.svg" alt="eCommerce HTML Template"/></a>
 					<button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 				</div>
 				<div className="offcanvas-body">
@@ -46,13 +46,13 @@ const PrimaryNav = () => {
 						<div className="collapse mt-2" id="collapseExample">
 							<div className="card card-body">
 								<ul className="mb-0 list-unstyled">
-									<li><a className="dropdown-item" href="./pages/shop-grid.html">Dairy, Bread &amp; Eggs</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-grid.html">Snacks &amp; Munchies</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-grid.html">Fruits &amp; Vegetables</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-grid.html">Cold Drinks &amp; Juices</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-grid.html">Breakfast &amp; Instant Food</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-grid.html">Bakery &amp; Biscuits</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-grid.html">Chicken, Meat &amp; Fish</a></li>
+									<li><a className="dropdown-item" href="/">Dairy, Bread &amp; Eggs</a></li>
+									<li><a className="dropdown-item" href="/">Snacks &amp; Munchies</a></li>
+									<li><a className="dropdown-item" href="/">Fruits &amp; Vegetables</a></li>
+									<li><a className="dropdown-item" href="/">Cold Drinks &amp; Juices</a></li>
+									<li><a className="dropdown-item" href="/">Breakfast &amp; Instant Food</a></li>
+									<li><a className="dropdown-item" href="/">Bakery &amp; Biscuits</a></li>
+									<li><a className="dropdown-item" href="/">Chicken, Meat &amp; Fish</a></li>
 								</ul>
 							</div>
 						</div>
@@ -70,13 +70,13 @@ const PrimaryNav = () => {
 							All Departments
 						</button>
 						<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-							<li><a className="dropdown-item" href="./pages/shop-grid.html">Dairy, Bread &amp; Eggs</a></li>
-							<li><a className="dropdown-item" href="./pages/shop-grid.html">Snacks &amp; Munchies</a></li>
-							<li><a className="dropdown-item" href="./pages/shop-grid.html">Fruits &amp; Vegetables</a></li>
-							<li><a className="dropdown-item" href="./pages/shop-grid.html">Cold Drinks &amp; Juices</a></li>
-							<li><a className="dropdown-item" href="./pages/shop-grid.html">Breakfast &amp; Instant Food</a></li>
-							<li><a className="dropdown-item" href="./pages/shop-grid.html">Bakery &amp; Biscuits</a></li>
-							<li><a className="dropdown-item" href="./pages/shop-grid.html">Chicken, Meat &amp; Fish</a></li>
+							<li><a className="dropdown-item" href="/">Dairy, Bread &amp; Eggs</a></li>
+							<li><a className="dropdown-item" href="/">Snacks &amp; Munchies</a></li>
+							<li><a className="dropdown-item" href="/">Fruits &amp; Vegetables</a></li>
+							<li><a className="dropdown-item" href="/">Cold Drinks &amp; Juices</a></li>
+							<li><a className="dropdown-item" href="/">Breakfast &amp; Instant Food</a></li>
+							<li><a className="dropdown-item" href="/">Bakery &amp; Biscuits</a></li>
+							<li><a className="dropdown-item" href="/">Chicken, Meat &amp; Fish</a></li>
 						</ul>
 					</div>
 					<div>
@@ -84,36 +84,36 @@ const PrimaryNav = () => {
 							<li className="nav-item dropdown w-100 w-lg-auto">
 								<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Home</a>
 								<ul className="dropdown-menu">
-									<li><a className="dropdown-item" href="./index.html">Home 1</a></li>
-									<li><a className="dropdown-item" href="./pages/index-2.html">Home 2</a></li>
-									<li><a className="dropdown-item" href="./pages/index-3.html">Home 3</a></li>
-									<li><a className="dropdown-item" href="./pages/index-4.html">Home 4</a></li>
+									<li><a className="dropdown-item" href="/">Home 1</a></li>
+									<li><a className="dropdown-item" href="/">Home 2</a></li>
+									<li><a className="dropdown-item" href="/">Home 3</a></li>
+									<li><a className="dropdown-item" href="/">Home 4</a></li>
 									<li>
-										<a className="dropdown-item" href="./pages/index-5.html">Home 5</a>
+										<a className="dropdown-item" href="/">Home 5</a>
 									</li>
 								</ul>
 							</li>
 							<li className="nav-item dropdown w-100 w-lg-auto">
 								<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
 								<ul className="dropdown-menu">
-									<li><a className="dropdown-item" href="./pages/shop-grid.html">Shop Grid - Filter</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-grid-3-column.html">Shop Grid - 3 column</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-list.html">Shop List - Filter</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-filter.html">Shop - Filter</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-fullwidth.html">Shop Wide</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-single.html">Shop Single</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-single-2.html">Shop Single v2</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-wishlist.html">Shop Wishlist</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-cart.html">Shop Cart</a></li>
-									<li><a className="dropdown-item" href="./pages/shop-checkout.html">Shop Checkout</a></li>
+									<li><a className="dropdown-item" href="/">Shop Grid - Filter</a></li>
+									<li><a className="dropdown-item" href="/">Shop Grid - 3 column</a></li>
+									<li><a className="dropdown-item" href="/">Shop List - Filter</a></li>
+									<li><a className="dropdown-item" href="/">Shop - Filter</a></li>
+									<li><a className="dropdown-item" href="/">Shop Wide</a></li>
+									<li><a className="dropdown-item" href="/">Shop Single</a></li>
+									<li><a className="dropdown-item" href="/">Shop Single v2</a></li>
+									<li><a className="dropdown-item" href="/">Shop Wishlist</a></li>
+									<li><a className="dropdown-item" href="/">Shop Cart</a></li>
+									<li><a className="dropdown-item" href="/">Shop Checkout</a></li>
 								</ul>
 							</li>
 							<li className="nav-item dropdown w-100 w-lg-auto">
 								<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Stores</a>
 								<ul className="dropdown-menu">
-									<li><a className="dropdown-item" href="./pages/store-list.html">Store List</a></li>
-									<li><a className="dropdown-item" href="./pages/store-grid.html">Store Grid</a></li>
-									<li><a className="dropdown-item" href="./pages/store-single.html">Store Single</a></li>
+									<li><a className="dropdown-item" href="/">Store List</a></li>
+									<li><a className="dropdown-item" href="/">Store Grid</a></li>
+									<li><a className="dropdown-item" href="/">Store Single</a></li>
 								</ul>
 							</li>
 							<li className="nav-item dropdown w-100 w-lg-auto dropdown-fullwidth">
@@ -122,36 +122,36 @@ const PrimaryNav = () => {
 									<div className="row p-2 p-lg-4">
 										<div className="col-lg-3 col-12 mb-4 mb-lg-0">
 											<h6 className="text-primary ps-3">Dairy, Bread &amp; Eggs</h6>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Butter</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Milk Drinks</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Curd &amp; Yogurt</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Eggs</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Buns &amp; Bakery</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Cheese</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Condensed Milk</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Dairy Products</a>
+											<a className="dropdown-item" href="/">Butter</a>
+											<a className="dropdown-item" href="/">Milk Drinks</a>
+											<a className="dropdown-item" href="/">Curd &amp; Yogurt</a>
+											<a className="dropdown-item" href="/">Eggs</a>
+											<a className="dropdown-item" href="/">Buns &amp; Bakery</a>
+											<a className="dropdown-item" href="/">Cheese</a>
+											<a className="dropdown-item" href="/">Condensed Milk</a>
+											<a className="dropdown-item" href="/">Dairy Products</a>
 										</div>
 										<div className="col-lg-3 col-12 mb-4 mb-lg-0">
 											<h6 className="text-primary ps-3">Breakfast &amp; Instant Food</h6>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Breakfast Cereal</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Noodles, Pasta &amp; Soup</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Frozen Veg Snacks</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Frozen Non-Veg Snacks</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Vermicelli</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Instant Mixes</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Batter</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Fruit and Juices</a>
+											<a className="dropdown-item" href="/">Breakfast Cereal</a>
+											<a className="dropdown-item" href="/">Noodles, Pasta &amp; Soup</a>
+											<a className="dropdown-item" href="/">Frozen Veg Snacks</a>
+											<a className="dropdown-item" href="/">Frozen Non-Veg Snacks</a>
+											<a className="dropdown-item" href="/">Vermicelli</a>
+											<a className="dropdown-item" href="/">Instant Mixes</a>
+											<a className="dropdown-item" href="/">Batter</a>
+											<a className="dropdown-item" href="/">Fruit and Juices</a>
 										</div>
 										<div className="col-lg-3 col-12 mb-4 mb-lg-0">
 											<h6 className="text-primary ps-3">Cold Drinks &amp; Juices</h6>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Soft Drinks</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Fruit Juices</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Coldpress</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Water &amp; Ice Cubes</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Soda &amp; Mixers</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Health Drinks</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Herbal Drinks</a>
-											<a className="dropdown-item" href="./pages/shop-grid.html">Milk Drinks</a>
+											<a className="dropdown-item" href="/">Soft Drinks</a>
+											<a className="dropdown-item" href="/">Fruit Juices</a>
+											<a className="dropdown-item" href="/">Coldpress</a>
+											<a className="dropdown-item" href="/">Water &amp; Ice Cubes</a>
+											<a className="dropdown-item" href="/">Soda &amp; Mixers</a>
+											<a className="dropdown-item" href="/">Health Drinks</a>
+											<a className="dropdown-item" href="/">Herbal Drinks</a>
+											<a className="dropdown-item" href="/">Milk Drinks</a>
 										</div>
 										<div className="col-lg-3 col-12 mb-4 mb-lg-0">
 											<div className="card border-0">
@@ -162,7 +162,7 @@ const PrimaryNav = () => {
 														<br/>
 														offer today.
 													</h5>
-													<a href="#" className="btn btn-primary btn-sm mt-3">Shop Now</a>
+													<a href="/" className="btn btn-primary btn-sm mt-3">Shop Now</a>
 												</div>
 											</div>
 										</div>
@@ -170,36 +170,36 @@ const PrimaryNav = () => {
 								</div>
 							</li>
 							<li className="nav-item dropdown w-100 w-lg-auto">
-								<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pages</a>
+								<a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">Pages</a>
 								<ul className="dropdown-menu">
-									<li><a className="dropdown-item" href="./pages/blog.html">Blog</a></li>
-									<li><a className="dropdown-item" href="./pages/blog-single.html">Blog Single</a></li>
-									<li><a className="dropdown-item" href="./pages/blog-category.html">Blog Category</a></li>
-									<li><a className="dropdown-item" href="./pages/about.html">About us</a></li>
-									<li><a className="dropdown-item" href="./pages/404error.html">404 Error</a></li>
-									<li><a className="dropdown-item" href="./pages/contact.html">Contact</a></li>
+									<li><a className="dropdown-item" href="/">Blog</a></li>
+									<li><a className="dropdown-item" href="/">Blog Single</a></li>
+									<li><a className="dropdown-item" href="/">Blog Category</a></li>
+									<li><a className="dropdown-item" href="/">About us</a></li>
+									<li><a className="dropdown-item" href="/">404 Error</a></li>
+									<li><a className="dropdown-item" href="/">Contact</a></li>
 								</ul>
 							</li>
 							<li className="nav-item dropdown w-100 w-lg-auto">
-								<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
+								<a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
 								<ul className="dropdown-menu">
-									<li><a className="dropdown-item" href="./pages/signin.html">Sign in</a></li>
-									<li><a className="dropdown-item" href="./pages/signup.html">Signup</a></li>
-									<li><a className="dropdown-item" href="./pages/forgot-password.html">Forgot Password</a></li>
+									<li><a className="dropdown-item" href="/">Sign in</a></li>
+									<li><a className="dropdown-item" href="/">Signup</a></li>
+									<li><a className="dropdown-item" href="/">Forgot Password</a></li>
 									<li className="dropdown-submenu dropend">
-										<a className="dropdown-item dropdown-list-group-item dropdown-toggle" href="#">My Account</a>
+										<a className="dropdown-item dropdown-list-group-item dropdown-toggle" href="/">My Account</a>
 										<ul className="dropdown-menu">
-											<li><a className="dropdown-item" href="./pages/account-orders.html">Orders</a></li>
-											<li><a className="dropdown-item" href="./pages/account-settings.html">Settings</a></li>
-											<li><a className="dropdown-item" href="./pages/account-address.html">Address</a></li>
-											<li><a className="dropdown-item" href="./pages/account-payment-method.html">Payment Method</a></li>
-											<li><a className="dropdown-item" href="./pages/account-notification.html">Notification</a></li>
+											<li><a className="dropdown-item" href="/">Orders</a></li>
+											<li><a className="dropdown-item" href="/">Settings</a></li>
+											<li><a className="dropdown-item" href="/">Address</a></li>
+											<li><a className="dropdown-item" href="/">Payment Method</a></li>
+											<li><a className="dropdown-item" href="/">Notification</a></li>
 										</ul>
 									</li>
 								</ul>
 							</li>
 							<li className="nav-item w-100 w-lg-auto">
-								<a className="nav-link" href="./dashboard/index.html">Dashboard</a>
+								<a className="nav-link" href="/">Dashboard</a>
 							</li>
 						</ul>
 					</div>
