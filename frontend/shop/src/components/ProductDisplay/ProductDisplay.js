35fe4811f3c875b42f9aa5698532cf8ba7f3c54a
eduardo-mw <eduardo@middleware.io>
2023-12-10 17:04:51 -0800

    update default quantity to 1

--- frontend/shop/src/components/ProductDisplay/ProductDisplay.js
+++ frontend/shop/src/components/ProductDisplay/ProductDisplay.js
@@ -11,103 +11,107 @@ import productImagePineapples from "../../assets/images/products/product-img-16.
 import productImagePopcorn from "../../assets/images/products/product-img-5.jpg";
 import productImageYogurt from "../../assets/images/products/product-img-6.jpg";
 
+const ProductDisplay = (props) => {
+  const products = [
+    {
+      productID: 1,
+      productCategory: "Snacks and Munchies",
+      productTitle: "Johns Chips",
+      productPrice: "100",
+      productImage: productImageNuttyChips,
+      quantity: 1,
+    },
+    {
+      productID: 2,
+      productCategory: "Fruits",
+      productTitle: "Organic Apples",
+      productPrice: "50",
+      productImage: productImageApples,
+      quantity: 1,
+    },
+    {
+      productID: 3,
+      productCategory: "Vegetables",
+      productTitle: "Beets",
+      productPrice: "80",
+      productImage: productImageBeets,
+      quantity: 1,
+    },
+    {
+      productID: 4,
+      productCategory: "Cereals",
+      productTitle: "Corn Flakes",
+      productPrice: "30",
+      productImage: productImageCornFlakes,
+      quantity: 1,
+    },
+    {
+      productID: 5,
+      productCategory: "Fruits",
+      productTitle: "Kiwis",
+      productPrice: "40",
+      productImage: productImageKiwis,
+      quantity: 1,
+    },
+    {
+      productID: 6,
+      productCategory: "Snacks and Munchies",
+      productTitle: "Lays Chips",
+      productPrice: "20",
+      productImage: productImageLays,
+      quantity: 1,
+    },
+    {
+      productID: 7,
+      productCategory: "Fruits",
+      productTitle: "Pineapples",
+      productPrice: "25",
+      productImage: productImagePineapples,
+      quantity: 1,
+    },
+    {
+      productID: 8,
+      productCategory: "Snacks and Munchies",
+      productTitle: "Popcorn",
+      productPrice: "15",
+      productImage: productImagePopcorn,
+      quantity: 1,
+    },
+    {
+      productID: 9,
+      productCategory: "Dairy",
+      productTitle: "Yogurt",
+      productPrice: "70",
+      productImage: productImageYogurt,
+      quantity: 1,
+    },
+    {
+      productID: 10,
+      productCategory: "Fruits",
+      productTitle: "Bananas",
+      productPrice: "10",
+      productImage: productImageBananas,
+      quantity: 1,
+    },
+  ];
 
-const ProductDisplay = () => {
-    const products = [
-        {
-            productID: 1,
-            productCategory: "Snacks and Munchies",
-            productTitle: "Johns Chips",
-            productPrice: "100",
-            productImage: productImageNuttyChips,
-        },
-        {
-            productID: 2,
-            productCategory: "Fruits",
-            productTitle: "Organic Apples",
-            productPrice: "50",
-            productImage: productImageApples,
-        },
-        {
-            productID: 3,
-            productCategory: "Vegetables",
-            productTitle: "Beets",
-            productPrice: "80",
-            productImage: productImageBeets,
-        },
-        {
-            productID: 4,
-            productCategory: "Cereals",
-            productTitle: "Corn Flakes",
-            productPrice: "30",
-            productImage: productImageCornFlakes,
-        },
-        {
-            productID: 5,
-            productCategory: "Fruits",
-            productTitle: "Kiwis",
-            productPrice: "40",
-            productImage: productImageKiwis,
-        },
-        {
-            productID: 6,
-            productCategory: "Snacks and Munchies",
-            productTitle: "Lays Chips",
-            productPrice: "20",
-            productImage: productImageLays,
-        },
-        {
-            productID: 7,
-            productCategory: "Fruits",
-            productTitle: "Pineapples",
-            productPrice: "25",
-            productImage: productImagePineapples,
-        },
-        {
-            productID: 8,
-            productCategory: "Snacks and Munchies",
-            productTitle: "Popcorn",
-            productPrice: "15",
-            productImage: productImagePopcorn,
-        },
-        {
-            productID: 9,
-            productCategory: "Dairy",
-            productTitle: "Yogurt",
-            productPrice: "70",
-            productImage: productImageYogurt,
-        },
-        {
-            productID: 10,
-            productCategory: "Fruits",
-            productTitle: "Bananas",
-            productPrice: "10",
-            productImage: productImageBananas,
-        },
-    ];
-
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12 mb-6">
-                    <h3 className="mb-0">Products</h3>
-                </div>
-            </div>
-
-            <div className="row g-4 row-cols-lg-5 row-cols-2 row-cols-md-3">
-                {products.map((product) => (
-                    <div className="col" key={product.productID}>
-                        <ProductCard
-                            productCategory={product.productCategory}
-                            productTitle={product.productTitle}
-                            productPrice={product.productPrice}
-                            productImage={product.productImage}
-                        />
-                    </div>
-                ))}
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mb-6">
+          <h3 className="mb-0">Products</h3>
         </div>
-    );
+      </div>
+
+      <div className="row g-4 row-cols-lg-5 row-cols-2 row-cols-md-3">
+        {products.map((product) => (
+          <div className="col" key={product.productID}>
+            <ProductCard product={product} userCart={props.userCart} setUserCart={props.setUserCart} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductDisplay;
